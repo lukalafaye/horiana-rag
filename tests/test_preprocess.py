081d202f7a83efcf534213eb6219bf740d9a01d5
Luka LAFAYE DE MICHEAUX <luka.lafaye_de_micheaux@telecom-sudparis.eu>
2024-07-25 17:40:49 -0700

    added synopsis extractor and update tests

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -46,41 +46,67 @@ def docx_paths():
     return studies_docx_path
 
 
+@pytest.fixture(scope="module")
+def synopsis_paths():
+    root_dir = os.path.join(os.path.dirname(__file__), "../redacted")
+    studies_synopsis_paths = {}
+
+    for dirpath, _, filenames in os.walk(root_dir):
+        for filename in filenames:
+            # Check if the file has the '-stat-report.pdf' suffix
+            if filename.endswith("-synopsis.docx"):
+                # Extract the subfolder name from the directory path
+                subfolder_name = os.path.basename(dirpath)
+                # Construct the path to the file
+                path = os.path.join(dirpath, filename)
+                # Store the path in the dictionary
+                studies_synopsis_paths[subfolder_name] = path
+
+    return studies_synopsis_paths
+
+
 def remove_file_if_exists(file_path):
     if os.path.exists(file_path):
         os.remove(file_path)
 
 
-def test_preprocess(pdf_paths, docx_paths):
+def test_preprocess(pdf_paths, docx_paths, synopsis_paths):
     output_path = "out.pkl"
     remove_file_if_exists(output_path)
+
+    print(synopsis_paths)
 
     for study, path in pdf_paths.items():
         pdf_path = path
         docx_path = docx_paths[study]
+        synopsis_path = synopsis_paths[study]
 
-        preprocess(pdf_path, docx_path, output_path)
+        preprocess(pdf_path, docx_path, synopsis_path, output_path)
 
         assert os.path.exists(output_path)
+
         with open(output_path, "rb") as f:
             data = pickle.load(f)
+
         assert isinstance(data, dict)
         assert data
 
         os.remove(output_path)
 
 
-def test_extract_tables_chunks(pdf_paths, docx_paths):
+def test_extract_tables_chunks(pdf_paths, docx_paths, synopsis_paths):
     output_path = "out.pkl"
     tables_path = "tables.pkl"
+
     remove_file_if_exists(output_path)
     remove_file_if_exists(tables_path)
 
     for study, path in pdf_paths.items():
         pdf_path = path
         docx_path = docx_paths[study]
+        synopsis_path = synopsis_paths[study]
 
-        preprocess(pdf_path, docx_path, output_path)
+        preprocess(pdf_path, docx_path, synopsis_path, output_path)
         extract_tables_chunks(output_path, tables_path)
 
         assert os.path.exists(tables_path)
